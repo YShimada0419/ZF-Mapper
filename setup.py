cc53b3093e5ef025cf57153b2e1f6bbccde07740
Tiryoh <tiryoh@gmail.com>
2019-02-04 06:42:53 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,3 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-setup(name='zfmapper',
-      version='0.0.1',
-      description='Python setuptools helloworld',
-      author='Name',
-      author_email='name@example.jp',
-      url='https://github.com/',
-      packages=find_packages(),
-      install_requires=["tifffile"]
-      entry_points="""
-      [console_scripts]
-      zfmapper = zfmapper.zfmapper:main
-      """,)
+setup()
